3d6a671bbd939b4390615e0407b5f439c2b252e8
katya <kotenokches@rambler.ru>
2022-06-01 20:12:41 +0500

    Первый коммит

---
+++ src/main/java/skypro/dolgova/DolgovaApplication.java
@@ -0,0 +1,13 @@
+package skypro.dolgova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolgovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolgovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skypro/dolgova/DolgovaApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.dolgova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolgovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
